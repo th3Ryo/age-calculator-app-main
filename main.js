ef9e148ba6161146d0d73629fdf51d57e3500b2f
th3Ryo <fabian_b.g@hotmail.com>
2023-05-09 18:48:34 -0500

    pendiente en js el tema de las letras rojas

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const inputMes = document.querySelector("#ingresoMes")
 const inputYear = document.querySelector("#ingresoYear")
 
  
-let resultado = {};
+let resultado = {
+    /* dia : 1,
+    mes : 1,
+    year : 1992, */
+};
 
 
 
@@ -12,16 +16,24 @@ let resultado = {};
  * */
 function ingresoValorDia () {
         const ingresoDia = parseInt(inputDia.value);
-        if (!isNaN(ingresoDia)) {
-            console.log(ingresoDia);
-            resultado.dia = fechaHoy.dia - ingresoDia;
-            console.log(resultado.dia);
-        }
+        if (!isNaN(ingresoDia) && ingresoDia>0 && ingresoDia < 32) {
+            
+                //resta de dia actual - dia nacimiento 
+                resultado.dia = fechaHoy.dia - ingresoDia;
+            }
+        else {
+                console.log = ("no valido")
+            }
+        ajuste()
+        
 }
 
 if (inputDia) {
     inputDia.addEventListener('change', ingresoValorDia) //leer el valor actualizado
 }
+
+
+
 /** 
  * !fin dia  
  * */
@@ -32,12 +44,13 @@ if (inputDia) {
 const ingresoMes =0;
 function ingresoValorMes () {
     const ingresoMes = parseInt(inputMes.value);
-    if (!isNaN(ingresoMes)) {
-        console.log(ingresoMes);
+    if (!isNaN(ingresoMes) && ingresoMes > 0 && ingresoMes < 13 ) {
+        //resta de mes actual - mes nacimiento 
         resultado.mes = fechaHoy.mes - ingresoMes;
-        console.log(resultado.mes);
-
+    } else {
+        console.log = ("no valido")
     }
+    ajuste()
 }
 
 
@@ -53,11 +66,14 @@ if (inputMes) {
  * */
 function ingresoValorYear () {
     const ingresoYear = parseInt(inputYear.value);
-    if (!isNaN(ingresoYear)) {
-        console.log(ingresoYear);
+    //el segundo && corresponde a verificar que el año de ingreso sea menor que la fecha ingresada
+    if (!isNaN(ingresoYear)&& fechaHoy.year>ingresoYear) {
+        //resta de año actual - año nacimiento 
         resultado.year = fechaHoy.year - ingresoYear;
-        console.log(resultado.year);
+    } else {
+        console.log = ("no valido")
     }
+    ajuste()
 }
 
 
@@ -94,8 +110,44 @@ formatoFechaHoy(fechaHoy)
  * !operaciones 
  * */
 
-function ajuste () {
-    Object.keys(resultado).length == 3
-    ? console.log("tiene 3")
-    : console.log("no tiene 3")
-}
+function ajuste() {
+    let tamañoFecha = Object.keys(resultado).length;
+    if (tamañoFecha == 3) {
+        
+        // verificar queconsole.log("tiene 3");
+        // en caso que los dias sean negativos
+        if (resultado.dia < 0) {
+        resultado.dia += 30;
+        resultado.mes -= 1;
+        }
+        // en caso que los dias sean negativos
+        if (resultado.mes < 0) {
+        resultado.mes += 12;
+        resultado.year -= 1;
+        }
+        // en caso que los dias sean negativos
+        if (resultado.year < 0) {
+        console.log("no es hace nada");
+        }
+
+        //cambiar textos de resultados
+        const pValorDia = document.querySelector('.diaResultado p');
+        if (pValorDia) {
+        pValorDia.innerText  = resultado.dia ;
+        }
+        const pValorMes = document.querySelector('.mesResultado p');
+        if (pValorMes) {
+        pValorMes.innerText  = resultado.mes ;
+        }
+        const pValoryear = document.querySelector('.yearResultado p');
+        if (pValoryear) {
+        pValoryear.innerText  = resultado.year ;
+        }
+
+    } 
+  }
+  /** 
+ * !operaciones 
+ * */
+  
+
